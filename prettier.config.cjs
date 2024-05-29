53f09c280f0035e7170607ebffea321c44e5ce8b
jsstoni <jsstoniha@gmail.com>
2024-05-29 10:48:19 -0400

    prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
