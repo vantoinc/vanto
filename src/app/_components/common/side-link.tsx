cec2537fd6e4422f26152a1b69c98bbe3ac2840e
jsstoni <jsstoniha@gmail.com>
2024-08-02 14:55:04 -0400

    style: active link

--- src/app/_components/common/side-link.tsx
+++ src/app/_components/common/side-link.tsx
@@ -1,5 +1,7 @@
+import { cn } from "@/utils/libs/utils";
 import { LucideIcon } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface Props {
   path: string;
@@ -8,10 +10,14 @@ interface Props {
 }
 
 export function SideLink({ path, title, Icon }: Props) {
+  const pathname = usePathname();
   return (
     <Link
       href={path}
-      className="flex items-center gap-2 rounded-md p-2 text-xl text-muted-foreground hover:text-yellow-400"
+      className={cn(
+        "flex items-center gap-2 rounded-md p-2 text-xl text-muted-foreground hover:text-yellow-300",
+        { "text-yellow-300": pathname === path },
+      )}
     >
       <Icon size={20} /> {title}
     </Link>
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   ChartLine,
   LayoutDashboardIcon,
