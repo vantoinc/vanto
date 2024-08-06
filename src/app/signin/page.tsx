68884e3f33401eb409375c0696f94a335d35b3da
jsstoni <jsstoniha@gmail.com>
2024-08-06 16:05:07 -0400

    refactor: remove GitHub provider

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,22 +1,7 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { signIn } from "next-auth/react";
-
 export default function SigninPage() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <Button
-        variant="ghost"
-        onClick={async () => {
-          await signIn("github", {
-            callbackUrl: "/dashboard",
-            redirect: false,
-          });
-        }}
-      >
-        Sign in with Github
-      </Button>
-    </main>
+    <main className="flex h-screen flex-col items-center justify-center"></main>
   );
 }
--- src/utils/types/env.ts
+++ src/utils/types/env.ts
@@ -2,8 +2,6 @@ import { z } from "zod";
 
 export const env = z.object({
   DATABASE_URL: z.string(),
-  GITHUB_ID: z.string(),
-  GITHUB_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   NEXTAUTH_SECRET: z.string(),
