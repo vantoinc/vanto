f400aecd5808782bac7060284195c48b789299ca
jsstoni <jsstoniha@gmail.com>
2024-08-07 22:12:57 -0400

    refactor: prisma schema folder

--- prisma/migrations/20240805204342_cascade_delete/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "variant" DROP CONSTRAINT "variant_product_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "variant" ADD CONSTRAINT "variant_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "catalog"("id") ON DELETE CASCADE ON UPDATE CASCADE;
