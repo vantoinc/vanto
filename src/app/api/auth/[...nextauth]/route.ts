eb759a0a97a2d153f105f7909f069cdd27898d4a
jsstoni <jsstoniha@gmail.com>
2024-08-07 22:09:35 -0400

    feat: auth prisma adapter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,23 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/utils/libs/prisma";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
   pages: {
     signIn: "/signin",
+    error: "/signin",
   },
 });
 
