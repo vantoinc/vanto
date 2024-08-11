183993c488e16f9f076a483a73ed479aa52c54eb
jsstoni <jsstoniha@gmail.com>
2024-08-11 19:18:57 -0400

    chore: include id in token

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,26 @@
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
-import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
+import NextAuth, { Session } from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   callbacks: {
+    jwt: ({ token, user }) => {
+      if (user && user.id) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session: ({ session, token }: { session: Session; token: JWT }) => {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
     authorized: async ({ auth }) => {
       return !!auth;
     },
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,5 +1,20 @@
 import { z } from "zod";
 import { env } from "./env";
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
 
 declare global {
   namespace NodeJS {
