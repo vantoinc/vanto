3ec1c6e191a91a90605a268384c497338dbc29dd
jsstoni <jsstoniha@gmail.com>
2024-08-12 16:36:16 -0400

    refactor: middleware auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,24 @@
-export { auth as middleware } from "@/lib/auth";
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
 
-export const config = { matcher: ["/manage/:path*", "/admin/:path*"] };
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+  const urlError = new URL("/", nextUrl.origin);
+
+  const publicPaths = ["/"];
+  if (
+    publicPaths.includes(nextUrl.pathname) ||
+    nextUrl.pathname.startsWith("/api/")
+  ) {
+    return NextResponse.next();
+  }
+
+  if (!isLoggedIn) {
+    return NextResponse.redirect(urlError);
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
