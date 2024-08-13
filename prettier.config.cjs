e65cb9165a7aad6d9ea3178bccce642bb6d55b93
jsstoni <jsstoniha@gmail.com>
2024-08-13 17:15:02 -0400

    perf: eslint and changes in dependencies

--- prettier.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  semi: true,
-  trailingComma: "all",
-  singleQuote: false,
-  jsxSingleQuote: false,
-  printWidth: 80,
-  tabWidth: 2,
-  plugins: ["prettier-plugin-tailwindcss"],
-};
