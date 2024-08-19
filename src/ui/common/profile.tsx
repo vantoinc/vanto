e95fa8a7d7fae80db6de6f51226bfbe7bf83b545
jsstoni <jsstoniha@gmail.com>
2024-08-19 12:20:58 -0400

    style: profile

--- src/ui/common/profile.tsx
+++ src/ui/common/profile.tsx
@@ -1,25 +1,20 @@
-"use client";
-
-import { useSession } from "next-auth/react";
+import { auth } from "@/lib/auth";
 import Image from "next/image";
 
-export function Profile(): JSX.Element {
-  const session = useSession();
-
+export async function Profile(): Promise<JSX.Element> {
+  const session = await auth();
   return (
     <div className="mt-auto flex items-center gap-3 border-t pt-2">
       <Image
-        src={session.data?.user?.image || ""}
+        src={session?.user?.image || ""}
         alt="user"
         width={80}
         height={80}
         className="size-8 rounded-md"
       />
-      <div>
-        <p>{session.data?.user?.name}</p>
-        <small className="text-muted-foreground">
-          {session.data?.user?.email}
-        </small>
+      <div className="leading-[1.2em]">
+        <p>{session?.user?.name}</p>
+        <small className="text-muted-foreground">{session?.user?.email}</small>
       </div>
     </div>
   );
