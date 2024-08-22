4d1203df1a7ff437805db32cc1d467430faf773c
jsstoni <jsstoniha@gmail.com>
2024-08-22 16:47:36 -0400

    style: darkmode

--- src/ui/common/profile.tsx
+++ src/ui/common/profile.tsx
@@ -1,21 +1,30 @@
 import { auth } from "@/lib/auth";
 import Image from "next/image";
+import { ToggleTheme } from "./toggle-theme";
 
 export async function Profile(): Promise<JSX.Element> {
   const session = await auth();
   return (
-    <div className="mt-auto flex items-center gap-3 border-t pt-2">
-      <Image
-        src={session?.user?.image || ""}
-        alt="user"
-        width={80}
-        height={80}
-        className="size-8 rounded-md"
-      />
-      <div className="leading-[1.2em]">
-        <p>{session?.user?.name}</p>
-        <small className="text-muted-foreground">{session?.user?.email}</small>
+    <>
+      <div className="mt-auto">
+        <ToggleTheme />
       </div>
-    </div>
+
+      <div className="flex items-center gap-3 border-t pt-2">
+        <Image
+          src={session?.user?.image || ""}
+          alt="user"
+          width={80}
+          height={80}
+          className="size-8 rounded-md"
+        />
+        <div className="leading-[1.2em]">
+          <p>{session?.user?.name}</p>
+          <small className="text-muted-foreground">
+            {session?.user?.email}
+          </small>
+        </div>
+      </div>
+    </>
   );
 }
--- src/ui/common/side-link.tsx
+++ src/ui/common/side-link.tsx
@@ -18,8 +18,8 @@ export function SideLink({ path, title, Icon }: Props): JSX.Element {
     <Link
       href={path}
       className={cn(
-        "flex items-center gap-3 rounded-md p-2 px-0 text-xl text-muted-foreground hover:text-yellow-300",
-        { "text-yellow-300": pathname === path },
+        "flex items-center gap-3 rounded-md p-2 px-0 text-xl text-muted-foreground hover:text-yellow-600 dark:hover:text-yellow-300",
+        { "text-yellow-600 dark:text-yellow-300": pathname === path },
       )}
     >
       <Icon size={26} /> {title}
