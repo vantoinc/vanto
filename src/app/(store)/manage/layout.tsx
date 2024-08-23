9f447e3aedee124686596f070d7d8759ff20533e
jsstoni <jsstoniha@gmail.com>
2024-08-23 12:56:17 -0400

    style: sidebar

--- src/app/(store)/manage/layout.tsx
+++ src/app/(store)/manage/layout.tsx
@@ -10,8 +10,8 @@ export default async function Layout({
   return (
     <>
       <div className="grid min-h-screen w-full grid-cols-[260px_1fr]">
-        <div className="sticky top-0 flex h-screen flex-col gap-2 overflow-y-auto border-r bg-muted/40 p-8">
-          <h1 className="mb-2 flex items-center gap-2 text-xl font-bold">
+        <div className="sticky top-0 flex h-screen flex-col gap-2 overflow-y-auto bg-zinc-50 dark:bg-muted/40 p-6 border-r">
+          <h1 className="mb-6 flex items-center gap-2 text-xl font-bold">
             <Image src="/icon.png" alt="tilby" width={30} height={30} />
           </h1>
 
