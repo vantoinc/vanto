b7d00f699bd231d1c42d48e6cabb71fcda1f8110
jsstoni <jsstoniha@gmail.com>
2024-08-30 15:30:42 -0400

    style: sidebar links

--- src/ui/common/side-link.tsx
+++ src/ui/common/side-link.tsx
@@ -19,9 +19,9 @@ export function SideLink({ path, title, Icon }: Props): JSX.Element {
     <Link
       href={path}
       className={cn(
-        "flex items-center gap-2 rounded-md p-2 text-muted-foreground hover:text-foreground",
+        "flex items-center gap-2 rounded-md p-2.5 text-muted-foreground hover:text-yellow-600",
         {
-          "bg-muted text-foreground shadow-sm shadow-zinc-300 dark:shadow-black":
+          "bg-muted/80 text-foreground text-yellow-600 shadow-sm shadow-zinc-300 dark:shadow-black/60":
             pathname === path,
         },
       )}
