2cd0792f3ca319dccde90525d2ef8e9bafd840a8
jsstoni <jsstoniha@gmail.com>
2024-08-30 15:30:09 -0400

    style: dark mode in the dashboard layout

--- src/app/(auth)/dashboard/layout.tsx
+++ src/app/(auth)/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "next-themes";
+
 import { Logo } from "@/ui/common/logo";
 import { Profile } from "@/ui/common/profile";
 import { Sidebar } from "@/ui/common/sidebar";
@@ -8,10 +10,10 @@ export default async function Layout({
   children: React.ReactNode;
 }): Promise<JSX.Element> {
   return (
-    <>
+    <ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
       <div className="grid min-h-screen w-full grid-cols-[260px_1fr]">
-        <div className="sticky top-0 flex h-screen flex-col gap-2 overflow-y-auto border-r bg-zinc-50 p-6 dark:bg-muted/40">
-          <h1 className="mb-6 flex items-center text-xl font-bold">
+        <div className="sticky top-0 flex h-screen flex-col gap-2 overflow-y-auto rounded-r-2xl border-r bg-zinc-50 p-4 dark:bg-muted/40">
+          <h1 className="my-4 flex items-center px-2 text-xl font-bold">
             <Logo /> anto
           </h1>
 
@@ -24,6 +26,6 @@ export default async function Layout({
           <div className="h-full rounded-xl p-4">{children}</div>
         </div>
       </div>
-    </>
+    </ThemeProvider>
   );
 }
