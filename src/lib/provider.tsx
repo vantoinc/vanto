027f4b1a094d213ca8fbbce8f3271e31cddb7eb8
jsstoni <jsstoniha@gmail.com>
2024-08-30 15:28:39 -0400

    style: removing dark mode from app layout

--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -1,20 +1,10 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export default function Provider({
   children,
 }: ThemeProviderProps): JSX.Element {
-  return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <SessionProvider>{children}</SessionProvider>
-    </NextThemesProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 }
